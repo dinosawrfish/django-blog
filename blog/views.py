54d2ba35c53d77a5e08d9d9a49794f3016b707c9
dinosawrfish <dinosawrfish@gmail.com>
2025-07-08 12:59:10 -0700

    add blog_detail view function

--- blog/views.py
+++ blog/views.py
@@ -17,3 +17,13 @@ def blog_category(request, category):
         "posts": posts,
     }
     return render(request, "blog/category.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog/detail.html", context)
